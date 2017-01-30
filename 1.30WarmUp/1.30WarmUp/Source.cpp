3eafc181d4cd2ce94908c4df8d4ddcb81bf63f8a
Dr. Mo <Jennifer_Moriarty@dpsk12.org>
2017-01-30 12:36:17 -0700

    Dr. Mo rocks

---
+++ 1.30WarmUp/1.30WarmUp/Source.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "did you hear about the two guys that stole a calendar?"<<endl<<endl;
+	cout << "they each got six months!!!";
+
+}
--- Fri1.27/Fri1.27/Source.cpp
+++ Fri1.27/Fri1.27/Source.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 using namespace std;
+
+class Utkus {
+	int width, height;
+public:
+	int getWidth();
+};
+
 int main() {
-	cout << "this is where your code goes";
+	list<Utkus*> listOfItems;
+	list<Item*>::iterator it;
+	Now I am using this code to create an item :
+
+	Item *p = new Item();
+	p->id = itemCrowbar;
+	listOfItems.push_back(p);
+}
+
+int Utkus::getWidth() {
+	return width;
+
 }
